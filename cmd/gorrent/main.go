2af76534499f1f9c5fba609a41b58b7aeea1a61d
Wyatt James <wyattsamueljames@gmail.com>
2025-12-01 22:08:43 -0700

    feat(bencode): add integer decoder

--- cmd/gorrent/main.go
+++ cmd/gorrent/main.go
@@ -9,15 +9,28 @@ import (
 
 
 func main() {
-	byteString := "16:thisshoulddecode"
-	_, _ = fmt.Printf("Original byte string: %s\n\n", byteString)
+	// byteString := "16:thisshoulddecode"
+	// _, _ = fmt.Printf("Original byte string: %s\n\n", byteString)
+	//
+	// rawBytes := []byte(byteString)
+	//
+	// decodedString, err := bencode.DecodeByteString(rawBytes)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// fmt.Printf("Decoded Info:\nLength: %d\nContents: %s\n\n", decodedString.Length, decodedString.Contents)
 
-	rawBytes := []byte(byteString)
 
-	decodedString, err := bencode.DecodeByteString(rawBytes)
+	byteInt := "i67e"
+	fmt.Printf("Raw Integer: %s\n\n", byteInt)
+
+	rawBytes := []byte(byteInt)
+
+	decodedInt, err := bencode.DecodeInteger(rawBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Decoded Info:\nLength: %d\nContents: %s\n", decodedString.Length, decodedString.Contents)
+	fmt.Printf("Decoded Integer: %d\n", *decodedInt)
 }
--- internal/bencode/bencoder.go
+++ internal/bencode/bencoder.go
@@ -18,19 +18,23 @@ func Decode(data []byte) {
 
 }
 
-func DecodeByteString(data []byte) (*ByteString, error) {
+type BencodeByteString struct {
+	Length   int64
+	Contents string
+}
+
+func DecodeByteString(data []byte) (*BencodeByteString, error) {
 	colonIndex := -1
 	for i, b := range data {
 		if b == ':' {
 			colonIndex = i
-			break;
+			break
 		}
 	}
 
 	if colonIndex == -1 {
 		return nil, errors.New("No colon found in byte string.")
 	}
-
 
 	length, err := strconv.Atoi(string(data[:colonIndex]))
 	if err != nil {
@@ -39,19 +43,56 @@ func DecodeByteString(data []byte) (*ByteString, error) {
 
 	contentStart := colonIndex + 1
 
-	if len(data) < contentStart + length {
+	if len(data) < contentStart+length {
 		return nil, errors.New("Byte string length greater than available data")
 	}
 
-	contents := string(data[contentStart:contentStart+length])
+	contents := string(data[contentStart : contentStart+length])
 
-	return &ByteString{
+	return &BencodeByteString{
 		Length:   int64(length),
 		Contents: contents,
 	}, nil
 }
 
-type ByteString struct {
-	Length   int64
-	Contents string
+func DecodeInteger(data []byte) (*int64, error) {
+	if len(data) < 3 {
+		return nil, errors.New("Integer data too short.")
+	}
+
+	if data[0] != 'i' {
+		return nil, errors.New("Expected 'i' at beginning of integer data.")
+	}
+
+	endIndex := -1
+	for i := 1; i < len(data); i++ {
+		if data[i] == 'e' {
+			endIndex = i
+			break
+		}
+	}
+
+	if endIndex == -1 {
+		return nil, errors.New("Expected 'e' at the end of integer.")
+	}
+
+	intBytes := data[1:endIndex]
+	if len(intBytes) == 0 {
+		return nil, errors.New("Empty integer value.")
+	}
+
+	if len(intBytes) > 1 && intBytes[0] == 0 {
+		return nil, errors.New("Invalid leading zero found in integer.")
+	}
+
+	if len(intBytes) > 2 && string(intBytes) == "-0" {
+		return nil, errors.New("Negative zero is invalid.")
+	}
+
+	value, err := strconv.ParseInt(string(intBytes), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &value, nil
 }
